f11849340ea6ca8e16c1f9fd1afe1798e9a09919
Sabin <sabinpanta11@gmail.com>
2024-01-15 18:07:38 +0545

    error message in assertion updated for resource share test

--- tests/integration/Owncloud/OcisPhpSdk/ResourceInviteTest.php
+++ tests/integration/Owncloud/OcisPhpSdk/ResourceInviteTest.php
@@ -70,21 +70,46 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
     public function testInviteUser(): void
     {
         $share = $this->fileToShare->invite($this->einstein, $this->viewerRole);
-        $this->assertNull($share->getExpiration());
-        $this->assertSame($this->fileToShare->getId(), $share->getResourceId());
+        $this->assertNull($share->getExpiration(), "Expiration date for sharing resource wasn't found to be null");
+        $this->assertSame(
+            $this->fileToShare->getId(),
+            $share->getResourceId(),
+            "ResourceId doesn't match with Shared ResourceId"
+        );
         $receivedShares = $this->getSharedWithMeWaitTillShareIsAccepted($this->einsteinOcis);
-        $this->assertCount(1, $receivedShares);
-        $this->assertSame($this->fileToShare->getName(), $receivedShares[0]->getName());
+        $this->assertCount(
+            1,
+            $receivedShares,
+            "Failed to share resource " . $this->fileToShare->getName() . " with Einstein"
+        );
+        $this->assertSame(
+            $this->fileToShare->getName(),
+            $receivedShares[0]->getName(),
+            "Expected resource name to be " . $this->fileToShare->getName() . " but found " . $receivedShares[0]->getName()
+        );
     }
 
     public function testInviteAnotherUser(): void
     {
         $this->fileToShare->invite($this->einstein, $this->viewerRole);
         $share = $this->fileToShare->invite($this->marie, $this->viewerRole);
-        $this->assertSame($this->fileToShare->getId(), $share->getResourceId());
+        $this->assertSame(
+            $this->fileToShare->getId(),
+            $share->getResourceId(),
+            "ResourceId doesn't match with Shared ResourceId"
+        );
         $receivedShares = $this->getSharedWithMeWaitTillShareIsAccepted($this->marieOcis);
-        $this->assertCount(1, $receivedShares);
-        $this->assertSame($this->fileToShare->getName(), $receivedShares[0]->getName());
+        $this->assertCount(
+            1,
+            $receivedShares,
+            "Failed to share resource " . $this->fileToShare->getName() . " with Marie"
+        );
+        $this->assertSame(
+            "to-share-test.txt",
+            $receivedShares[0]->getName(),
+            "Expected resource name to be " . $this->fileToShare->getName()
+            . " but found " . $receivedShares[0]->getName()
+        );
     }
 
     public function testInviteGroup(): void
@@ -96,10 +121,23 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
         $this->createdGroups = [$philosophyHatersGroup];
         $philosophyHatersGroup->addUser($this->einstein);
         $share = $this->fileToShare->invite($philosophyHatersGroup, $this->viewerRole);
-        $this->assertSame($this->fileToShare->getId(), $share->getResourceId());
+        $this->assertSame(
+            $this->fileToShare->getId(),
+            $share->getResourceId(),
+            "ResourceId doesn't match with Shared ResourceId"
+        );
         $receivedShares = $this->getSharedWithMeWaitTillShareIsAccepted($this->einsteinOcis);
-        $this->assertCount(1, $receivedShares);
-        $this->assertSame($this->fileToShare->getName(), $receivedShares[0]->getName());
+        $this->assertCount(
+            1,
+            $receivedShares,
+            "Failed to share resource " . $this->fileToShare->getName() . " with group "
+        );
+        $this->assertSame(
+            $this->fileToShare->getName(),
+            $receivedShares[0]->getName(),
+            "Expected resource name to be " . $this->fileToShare->getName()
+            . " but found " . $receivedShares[0]->getName()
+        );
     }
 
     public function testInviteSameUserAgain(): void
@@ -124,12 +162,38 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
     {
         $tomorrow = new \DateTimeImmutable('tomorrow');
         $share = $this->fileToShare->invite($this->einstein, $this->viewerRole, $tomorrow);
-        $this->assertInstanceOf(\DateTimeImmutable::class, $share->getExpiration());
-        $this->assertSame($tomorrow->getTimestamp(), $share->getExpiration()->getTimestamp());
+        $shareResourceExpirationDateTime = $share->getExpiration();
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $shareResourceExpirationDateTime,
+            "Expected class to be 'DateTimeImmutable' but found "
+            . print_r($shareResourceExpirationDateTime, true)
+        );
+        $this->assertSame(
+            $tomorrow->getTimestamp(),
+            $shareResourceExpirationDateTime->getTimestamp(),
+            "Expected timestamp of created share of resource to be " . $tomorrow->getTimestamp() . " but found "
+            . $shareResourceExpirationDateTime->getTimestamp()
+        );
         $createdShares = $this->ocis->getSharedByMe();
-        $this->assertCount(1, $createdShares);
-        $this->assertInstanceOf(\DateTimeImmutable::class, $createdShares[0]->getExpiration());
-        $this->assertSame($tomorrow->getTimestamp(), $createdShares[0]->getExpiration()->getTimestamp());
+        $this->assertCount(
+            1,
+            $createdShares,
+            $this->fileToShare->getName() . " couldn't be shared"
+        );
+        $createdSharesExpirationDateTime =  $createdShares[0]->getExpiration();
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $createdSharesExpirationDateTime,
+            "Expected class to be 'DateTimeImmutable' but found "
+            . print_r($createdSharesExpirationDateTime, true)
+        );
+        $this->assertSame(
+            $tomorrow->getTimestamp(),
+            $createdSharesExpirationDateTime->getTimestamp(),
+            "Expected timestamp of shared resource to be " . $tomorrow->getTimestamp() . " but found "
+            . $createdSharesExpirationDateTime->getTimestamp()
+        );
     }
 
     public function testInviteWithPastExpiry(): void
@@ -143,16 +207,48 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
     {
         $expiry = new \DateTimeImmutable('2060-01-01 12:00:00', new \DateTimeZone('Europe/Kyiv'));
         $share = $this->fileToShare->invite($this->marie, $this->viewerRole, $expiry);
-        $this->assertInstanceOf(\DateTimeImmutable::class, $share->getExpiration());
+        $shareResourceExpirationDateTime = $share->getExpiration();
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $shareResourceExpirationDateTime,
+            "Expected class to be 'DateTimeImmutable' but found "
+            . print_r($shareResourceExpirationDateTime, true)
+        );
         // The returned expiry is in UTC timezone (2 hours earlier than the expiry time in Kyiv)
-        $this->assertSame("Thu, 01 Jan 2060 10:00:00 +0000", $share->getExpiration()->format('r'));
-        $this->assertSame("Z", $share->getExpiration()->getTimezone()->getName());
+        $this->assertSame(
+            "Thu, 01 Jan 2060 10:00:00 +0000",
+            $shareResourceExpirationDateTime->format('r'),
+            "Expected expiration datetime of shared resource doesn't match"
+        );
+        $this->assertSame(
+            "Z",
+            $shareResourceExpirationDateTime->getTimezone()->getName(),
+            "Expected timezone to be Z but found " . $shareResourceExpirationDateTime->getTimezone()->getName()
+        );
         $createdShares = $this->ocis->getSharedByMe();
-        $this->assertCount(1, $createdShares);
-        $this->assertInstanceOf(\DateTimeImmutable::class, $createdShares[0]->getExpiration());
+        $this->assertCount(
+            1,
+            $createdShares,
+            "Expected share created to be 1 but found " . count($createdShares)
+        );
+        $createdSharesExpirationDateTime = $createdShares[0]->getExpiration();
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $createdSharesExpirationDateTime,
+            "Expected class to be 'DateTimeImmutable' but found "
+            . print_r($createdSharesExpirationDateTime, true)
+        );
         // The returned expiry is in UTC timezone (2 hours earlier than the expiry time in Kyiv)
-        $this->assertSame("Thu, 01 Jan 2060 10:00:00 +0000", $createdShares[0]->getExpiration()->format('r'));
-        $this->assertSame("Z", $createdShares[0]->getExpiration()->getTimezone()->getName());
+        $this->assertSame(
+            "Thu, 01 Jan 2060 10:00:00 +0000",
+            $createdSharesExpirationDateTime->format('r'),
+            "Expected expiration datetime of created share of resource doesn't match "
+        );
+        $this->assertSame(
+            "Z",
+            $createdSharesExpirationDateTime->getTimezone()->getName(),
+            "Expected timezone to be Z but found " . $createdSharesExpirationDateTime->getTimezone()->getName()
+        );
     }
 
     public function testGetReceiversOfShareCreatedByInvite(): void
@@ -167,9 +263,21 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
         $shares[] = $this->fileToShare->invite($this->einstein, $this->viewerRole);
         $shares[] = $this->fileToShare->invite($this->marie, $this->viewerRole);
         $shares[] = $this->fileToShare->invite($philosophyHatersGroup, $this->viewerRole);
-        $this->assertSame("Albert Einstein", $shares[0]->getReceiver()->getDisplayName());
-        $this->assertSame("Marie Curie", $shares[1]->getReceiver()->getDisplayName());
-        $this->assertSame("philosophyhaters", $shares[2]->getReceiver()->getDisplayName());
+        $this->assertSame(
+            "Albert Einstein",
+            $shares[0]->getReceiver()->getDisplayName(),
+            "Expected receiver display name to be Albert Einstein but found " . $shares[0]->getReceiver()->getDisplayName()
+        );
+        $this->assertSame(
+            "Marie Curie",
+            $shares[1]->getReceiver()->getDisplayName(),
+            "Expected receiver display name to be Marie Curie but found " . $shares[1]->getReceiver()->getDisplayName()
+        );
+        $this->assertSame(
+            "philosophyhaters",
+            $shares[2]->getReceiver()->getDisplayName(),
+            "Expected receiver display name to be philosophyhaters but found " . $shares[2]->getReceiver()->getDisplayName()
+        );
     }
 
     public function testGetReceiversOfShareReturnedBySharedByMe(): void
@@ -187,16 +295,30 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
         $this->folderToShare->invite($this->marie, $this->viewerRole);
         $this->folderToShare->invite($philosophyHatersGroup, $this->viewerRole);
         $shares = $this->ocis->getSharedByMe();
-        $this->assertCount(6, $shares);
+        $this->assertCount(
+            6,
+            $shares,
+            "Expected count of shared resources to be 6 but found " . count($shares)
+        );
         for($i = 0; $i < 6; $i++) {
-            $this->assertInstanceOf(ShareCreated::class, $shares[$i]);
-            $this->assertSame($this->personalDrive->getId(), $shares[$i]->getDriveId());
+            $this->assertInstanceOf(
+                ShareCreated::class,
+                $shares[$i],
+                "Expected class to be 'ShareCreated' but found "
+                . get_class($shares[$i])
+            );
+            $this->assertSame(
+                $this->personalDrive->getId(),
+                $shares[$i]->getDriveId(),
+                "Driveid doesn't match"
+            );
             $this->assertThat(
                 $shares[$i]->getResourceId(),
                 $this->logicalOr(
                     $this->equalTo($this->folderToShare->getId()),
                     $this->equalTo($this->fileToShare->getId())
-                )
+                ),
+                "Resource Id doesn't match"
             );
             $this->assertThat(
                 $shares[$i]->getReceiver()->getDisplayName(),
@@ -204,7 +326,9 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
                     $this->equalTo("philosophyhaters"),
                     $this->equalTo("Marie Curie"),
                     $this->equalTo("Albert Einstein")
-                )
+                ),
+                "Expected display name of Receiver be either philosophyhaters,Marie Curie or Albert Einstein
+                but found " . $shares[$i]->getReceiver()->getDisplayName()
             );
 
         }
@@ -224,10 +348,15 @@ class ResourceInviteTest extends OcisPhpSdkTestCase
          * @var ShareReceived $receivedShare
          */
         $receivedShare = $this->getSharedWithMeWaitTillShareIsAccepted($this->marieOcis)[0];
-        $this->assertSame('to-share-test.txt', $receivedShare->getName());
+        $this->assertSame(
+            'to-share-test.txt',
+            $receivedShare->getName(),
+            "Expected shared resource to be 'to-share-test.txt' but found " . $receivedShare->getName()
+        );
         $this->assertSame(
             'some content',
-            $this->marieOcis->getResourceById($receivedShare->getRemoteItemId())->getContent()
+            $this->marieOcis->getResourceById($receivedShare->getRemoteItemId())->getContent(),
+            "File content doesn't match"
         );
     }
 }
