8f1bcb3bfc16c43443f9c5a2da88a069dd6f2585
Ashim Shrestha <ashimshrestha2384@gmail.com>
2025-12-30 17:04:18 +0545

    test: test drive test setDescription
    Signed-off-by: Ashim Shrestha <ashimshrestha2384@gmail.com>

--- tests/integration/Owncloud/OcisPhpSdk/DriveTest.php
+++ tests/integration/Owncloud/OcisPhpSdk/DriveTest.php
@@ -108,6 +108,26 @@ class DriveTest extends OcisPhpSdkTestCase
     }
 
     /**
+     * @dataProvider getTestStrings
+     */
+    public function testSetDescription(string $description): void
+    {
+        $this->drive->setDescription($description);
+        $this->assertEquals($this->drive->getDescription(), $description, "Failed to set description $description");
+    }
+
+    /**
+     * @return array<int, array<int, string>>
+     */
+    public static function getTestStrings(): array
+    {
+        return [
+            [''],
+            ['test string'],
+          ];
+    }
+
+    /**
      * @throws ForbiddenException
      * @throws InvalidResponseException
      * @throws BadRequestException
